1cacd993d0d5d37aefffdc37a0568a0b68b38164
Khyati Desai <khyati@intalio.com>
2012-10-05 18:31:46 +0530

    Adding java folder

---
+++ src/main/java/org/intalio/ldaphelper/ExtensionMapper.java
@@ -0,0 +1,52 @@
+/**
+ * ExtensionMapper.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis2 version: 1.5.6  Built on : Aug 30, 2011 (10:01:01 CEST)
+ */
+
+package org.intalio.ldaphelper;
+
+/**
+ * ExtensionMapper class
+ */
+
+public class ExtensionMapper {
+
+	public static java.lang.Object getTypeObject(java.lang.String namespaceURI,
+			java.lang.String typeName, javax.xml.stream.XMLStreamReader reader)
+			throws java.lang.Exception {
+
+		if ("http://bpms.intalio.org/LdapHelper/".equals(namespaceURI)
+				&& "OperationAction".equals(typeName)) {
+
+			return org.intalio.ldaphelper.OperationAction.Factory.parse(reader);
+
+		}
+
+		if ("http://bpms.intalio.org/LdapHelper/".equals(namespaceURI)
+				&& "Item".equals(typeName)) {
+
+			return org.intalio.ldaphelper.Item.Factory.parse(reader);
+
+		}
+
+		if ("http://bpms.intalio.org/LdapHelper/".equals(namespaceURI)
+				&& "Operation".equals(typeName)) {
+
+			return org.intalio.ldaphelper.Operation.Factory.parse(reader);
+
+		}
+
+		if ("http://bpms.intalio.org/LdapHelper/".equals(namespaceURI)
+				&& "Data".equals(typeName)) {
+
+			return org.intalio.ldaphelper.Data.Factory.parse(reader);
+
+		}
+
+		throw new org.apache.axis2.databinding.ADBException("Unsupported type "
+				+ namespaceURI + " " + typeName);
+	}
+
+}
